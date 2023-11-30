b7376183ef2724a3bccebc0008872295f287a957
DavidJLion <dalarconp12@yahoo.com>
2023-11-30 10:53:49 -0300

    Initial commit

--- main.R
+++ main.R
@@ -1,6 +1,36 @@
 library("gitcreds")
 library("gert")
 library("gh")
+library("usethis")
+library("postcards") 
+
+
+create_github_token()
+
+gitcreds_set()
+
+edit_git_config()
+
+use_git()
+
+
+use_github()
+
+
+git_add("prueba-here.R")
+
+git_commit("se subio archivo prueba")
+
+git_push()
+
+
+###------------------------
+
+git_add("rcars.R")
+git_commit("se subio una tabla derivada de Rcars")
+git_push()
+
+###-----------------------
 
 
 
